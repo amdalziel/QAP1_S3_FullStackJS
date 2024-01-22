c4380869731824e1585f983f678c5665952c6da8
Amy Dalziel <amy.dalziel@keyin.com>
2024-01-22 12:45:02 -0330

    Task1 Http - img now working

--- task1_http.js
+++ task1_http.js
@@ -9,11 +9,60 @@
 
 // Code: 
 
-const http = require('http'); 
-const port = 8080; 
+// Three core packages required for this code - the three packages explored in Task 1!
+const http = require('http');
+const fs = require('fs');
+const path = require('path'); 
 
-http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'}); 
-    res.write('<h1>Hello World</h1>'); 
-    res.end(); // End the response 
-}).listen(port); 
+// Initialize variables for the port and the source of images for the website 
+const port = 8080;
+const imgFolder = './Images/original_images/';
+
+fs.readdir(imgFolder, (error, files) => {
+
+    // Initialize an empty array to store the complete path for each image 
+    let imgArray = [];
+
+    // Initialize a variable to store the complete path for an image (using path.join)
+    let imgUrl = "";
+
+    // Write an error to the console if the operation fails 
+    if (error) {
+        console.log("Error - files not located.")
+    } else {
+        let imgCount = 0; 
+
+    // Read the directory of "imgFolder" - for each file join the imgFolder path and file name in 
+    // a single variable "imgUrl" and store it in the imgArray. 
+        files.forEach((file) => {
+            imgUrl = path.join(imgFolder, file); 
+            console.log(`Image ${imgCount}: ${imgUrl}`); 
+            imgArray.push(imgUrl);
+
+            imgCount ++; 
+        });
+
+       
+        http.createServer((req, res) => {
+            fetchFile(imgArray[2], res);
+        }).listen(port, () => {
+            console.log(`Server running at http://localhost:${port}/`);
+        });
+    }
+
+    // Verify that the imgArray has the complete list of file names 
+    console.log(imgArray);
+});
+
+function fetchFile(path, response) {
+    fs.readFile(path, (error, content) => {
+        if (error) {
+            response.writeHead(500, { 'Content-Type': 'text/html' });
+            response.end('500 Internal Error');
+        } else {
+            response.writeHead(200, { 'Content-Type': 'image/jpeg' });
+            response.end(content, 'binary');
+        }
+    });
+}
+
