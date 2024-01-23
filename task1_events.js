a2f1f2f68d5293e744c90e8d038cf97814912377
Amy Dalziel <amy.dalziel@keyin.com>
2024-01-23 14:42:24 -0330

    changes to task1_events

--- task1_events.js
+++ task1_events.js
@@ -12,31 +12,32 @@
 
 // Part 1: Using .on and .emit methods: 
 const EventEmitter = require('events'); 
-let eventEmitter = new EventEmitter(); 
+class MyEmitter extends EventEmitter{}; 
+const myEmitter = new MyEmitter(); 
 
 // Initialize a counter to use to number events 
 let steps=0; 
 
 
 // First Event - welcome user to the store 
-eventEmitter.on('event', sayHello =()=> {
+myEmitter.on('event', sayHello =()=> {
     steps ++; 
     console.log(`#${steps}: Hello! Welcome to our website!`)}); 
 
 // Second Event - browsing message 
-eventEmitter.on('event', browseFeatures =()=> {
+myEmitter.on('event', browseFeatures =()=> {
     steps ++; 
     console.log(`#${steps}: Browse our website for current deals. Our prices won't be beat!`)
 } ); 
 
 // Third Event - leave a comment 
-eventEmitter.on('event', leaveComment =()=> {
+myEmitter.on('event', leaveComment =()=> {
     steps ++; 
     console.log(`#${steps}: Leave a comment below for a chance to win a $1,000 gift card.`)
 } ); 
 
 // Fourth Event - list current important reminders 
-eventEmitter.on('event', finalReminders =(...args)=> {
+myEmitter.on('event', finalReminders =(...args)=> {
     steps ++; 
     const reminders = args.join('\n* '); 
     console.log(`#${steps}: Store reminders: \n* ${reminders}. \nHave a great day!`); 
@@ -46,11 +47,11 @@ console.log("Part 1: Using .on and .emit methods:");
 console.log();
 
   
-// An array of the number of listeners for the 'event' (In this case, FOUR events)
-  console.log(eventEmitter.listeners('event'));
+// An array of the listeners for the 'event' (In this case, FOUR events)
+  console.log(myEmitter.listeners('event'));
 
 // Raise the 'event' - and pass through the args needed for the last event (different deals)  
-  eventEmitter.emit('event', 'No returns after 30 days', 'Free shipping ends on January 31st', 'All shirts - Buy one, Get one free');
+  myEmitter.emit('event', 'No returns after 30 days', 'Free shipping ends on January 31st', 'All shirts - Buy one, Get one free');
   console.log(); 
 
 
